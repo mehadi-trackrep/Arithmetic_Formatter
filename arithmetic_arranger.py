fc922dbbdc8973de3d216b6d44ab61896fbe9d87
mehadi.hasan <mehadi.hasan@goava.com>
2023-05-03 20:03:38 +0600

    Implement the display param requirement

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,31 +1,68 @@
+import re
 from arithmatic_problem_validator import ArithmaticProblemValidator
+
+FOUR_WHITE_SPACES = '    ' ## 4 spaces
 
 def arithmetic_arranger(problems: list[str], display=False) -> str:
     
-    print(problems)
-    print("\n")
-
     arithmatic_problem_validator_obj = ArithmaticProblemValidator(problems)
     
     if arithmatic_problem_validator_obj.is_too_many_problems():
         return 'Error: Too many problems.'
     elif arithmatic_problem_validator_obj.is_inappropriate_operators():
-        return 'Error: Operator must be '+' or '-'.'
+        return "Error: Operator must be '+' or '-'."
     elif arithmatic_problem_validator_obj.is_non_digits():
         return 'Error: Numbers must only contain digits.'
     elif arithmatic_problem_validator_obj.is_over_size_of_any_operand():
         return 'Error: Numbers cannot be more than four digits.'
     
-    s1 = '  32         1      9999      523'
-    s2 = '+  8    - 3801    + 9999    -  49'
-    s3 = '----    ------    ------    -----'
-    s4 = '  40     -3800     19998      474'
+    """
+        s1 = '  32         1      9999      523'
+        s2 = '+  8    - 3801    + 9999    -  49'
+        s3 = '----    ------    ------    -----'
+        s4 = '  40     -3800     19998      474'
+    """
 
-    '''
-        ## gap er space - 4 spaces
-            
-    '''
+    s1 = ''
+    s2 = ''
+    s3 = ''
+    s4 = ''
 
-    final_str = f"{s1}\n{s2}\n{s3}\n{s4}"
-    # print("\n==> final_str: \n", final_str)
-    return final_str
+    flag = False
+
+    for each_problem in problems:
+        temp = re.sub(r'\s', '', each_problem)
+        splitted_list = temp.split('+') if '+' in temp else temp.split('-')
+        
+        if display:
+            arithmatic_result = str(int(splitted_list[0]) + int(splitted_list[1])) if '+' in temp \
+                            else str(int(splitted_list[0]) - int(splitted_list[1]))
+        big_operand_len = len(splitted_list[0]) if len(splitted_list[0]) >= len(splitted_list[1]) \
+                        else len(splitted_list[1])
+        
+        mx_len = big_operand_len+2
+
+        if not flag:
+            s1 = ' '*(mx_len - len(splitted_list[0])) + splitted_list[0]
+            s2 = ('+' if '+' in temp else '-') + ' '*(mx_len - len(splitted_list[1]) - 1) + splitted_list[1]
+            s3 = '-'*mx_len
+            if display:
+                s4 = ' '*(mx_len - len(arithmatic_result)) + arithmatic_result
+            flag = True
+        else:
+            s1 = s1 + FOUR_WHITE_SPACES + ' '*(mx_len - len(splitted_list[0])) + splitted_list[0]
+            s2 = s2 + FOUR_WHITE_SPACES + ('+' if '+' in temp else '-') + \
+                ' '*(mx_len - len(splitted_list[1]) - 1) + splitted_list[1]
+            s3 = s3 + FOUR_WHITE_SPACES + '-'*mx_len
+            if display:
+                s4 = s4 + FOUR_WHITE_SPACES + ' '*(mx_len - len(arithmatic_result)) + arithmatic_result
+
+        # print(f"{s1}\n{s2}\n{s3}\n{s4}")
+        # print(arithmatic_result)
+        # print(big_operand_len)
+        # print("############")
+
+    if display:
+        return f"{s1}\n{s2}\n{s3}\n{s4}"
+    else:
+        return f"{s1}\n{s2}\n{s3}"
--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from arithmetic_arranger import arithmetic_arranger
+
 if __name__=='__main__':
-    print(arithmetic_arranger(["32 + 8", "1 - 3801", "9999 + 9999", "523 - 49"], True))
-    # print(arithmetic_arranger(["32 + 8", "1 - 3801", "9999 + 9999", "44 - 44", "mehadi"], True))
-    # print(arithmetic_arranger(["32 + 8", "1 - 3801", "9999 + 9999", "523 / 49", "44 - 44"], True))
-    # print(arithmetic_arranger(["32 + 8", "1 - 3801", "9999 + 99991", "523 - 49"], True))
+    all_problems = [
+        ["32 + 8", "1 - 3801", "9999 + 9999", "523 - 49"]
+        ,["32 + 8", "1 - 3801", "9999 + 9999", "44 - 44", "mehadi"]
+        ,["32 + 8", "1 - 3801", "9999 + 9999", "523 / 49", "44 - 44"]
+        ,["32 + 8", "1 - 3801", "9999 + 99991", "523 - 49"]
+    ]
+    for each_problem_set in all_problems:
+        print(each_problem_set)
+        print(arithmetic_arranger(each_problem_set, True))
+        print(arithmetic_arranger(each_problem_set))
+        print("======\t======\t======\t======\t======\t======\n")
